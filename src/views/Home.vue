212185af58245c269c72b8bfa0b9bfba40fbcea6
hyounoo <hyounoosung@gmail.com>
2021-08-08 22:38:21 +0900

    downgrage to vue2.0

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,13 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
-@Options({
+@Component({
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 })
 export default class Home extends Vue {}
 </script>
