43cfa47caa35e001f15ecf83f8134cb889f2c47d
hyounoo <hyounoosung@gmail.com>
2021-08-08 23:17:13 +0900

    Add Vuefire

---
+++ src/firebase.ts
@@ -0,0 +1,29 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
+}
+
+firebase.initializeApp(firebaseConfig)
+
+// utils
+const db = firebase.firestore()
+const auth = firebase.auth()
+
+// collection references
+const usersCollection = db.collection('users')
+const postsCollection = db.collection('posts')
+const commentsCollection = db.collection('comments')
+const likesCollection = db.collection('likes')
+const { Timestamp, GeoPoint } = firebase.firestore
+
+// export utils/refs
+export { db, auth, usersCollection, postsCollection, commentsCollection, likesCollection, Timestamp, GeoPoint }
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { auth } from './firebase'
+import { firestorePlugin } from 'vuefire'
 
 Vue.config.productionTip = false
+Vue.use(firestorePlugin)
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App)
-}).$mount('#app')
+let app: Vue
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch('fetchUserProfile', user)
+  }
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: (h) => h(App)
+    }).$mount('#app')
+  }
+})
