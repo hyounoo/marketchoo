af945d880fc5175d4a3fc259465ce99a2aeb5db0
nasangw <nasangw@wemade.com>
2021-08-12 18:19:26 +0900

    vuetify test를 위한 임시 세팅

---
+++ test/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <!--  -->
+      <ul>
+        <li><a href="##">Link</a></li>
+      </ul>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+      <h2>heading2</h2>
+      <h3>heading3</h3>
+      <h4>heading4</h4>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
+</script>
