0c1f6d4d303cb4ddcf96e2e4646876786368fadd
nasangw <good617boy@gmail.com>
2021-08-29 00:45:52 +0900

    - sass 설치 및 적용 - tailwind가 적용된 globals.css 해제

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import '../assets/sass/app.scss';
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
