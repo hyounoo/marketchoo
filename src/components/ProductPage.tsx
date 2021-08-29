865bc7338b46222e665c235d012369906661b10b
hyounoo <hyounoosung@gmail.com>
2021-08-29 18:01:50 +0900

    Update ProductPage.tsx

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -16,7 +16,7 @@ type Product = {
 function ProductPage({ product }: { product: Product }) {
   const [count, setCount] = useState(1)
   const handleCount = (value: any) => (!(count === 0 && value === -1) ? setCount(count + value) : count)
-  const { title, defaultProductVariant, mainImage, body } = product
+  // const { title, defaultProductVariant, mainImage, body } = product
 
   return (
     <div className="container mx-auto px-6">
@@ -24,13 +24,13 @@ function ProductPage({ product }: { product: Product }) {
         <div className="w-full h-64 md:w-1/2 lg:h-96">
           <img
             className="h-full w-full rounded-md object-cover max-w-lg mx-auto"
-            src={urlFor(mainImage).auto('format').width(1051).fit('crop').quality(80).toString()!}
-            alt={mainImage?.alt || `Photo of ${title}`}
+            src={urlFor(product.mainImage).auto('format').width(1051).fit('crop').quality(80).toString()!}
+            alt={product.mainImage?.alt || `Photo of ${product.title}`}
           />
         </div>
         <div className="w-full max-w-lg mx-auto mt-5 md:ml-8 md:mt-0 md:w-1/2">
-          <h3 className="text-gray-700 uppercase text-lg">{title}</h3>
-          <span className="text-gray-500 mt-3">${defaultProductVariant?.price}</span>
+          <h3 className="text-gray-700 uppercase text-lg">{product.title}</h3>
+          <span className="text-gray-500 mt-3">${product.defaultProductVariant?.price}</span>
           <hr className="my-3" />
           <div className="mt-2">
             <label className="text-gray-700 text-sm" htmlFor="count">
@@ -88,7 +88,7 @@ function ProductPage({ product }: { product: Product }) {
       </div>
       <div className="mt-16 md:w-2/3">
         <h3 className="text-gray-600 text-2xl font-medium">Description</h3>
-        {body && <PortableText blocks={body?.en} className="text-gray-600" />}
+        {product.body && <PortableText blocks={product.body?.en} className="text-gray-600" />}
       </div>
     </div>
   )
