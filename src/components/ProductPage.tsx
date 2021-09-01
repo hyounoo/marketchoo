7da89aca7729321832b94044dfec6d03e2bfc3e0
hyounoo <hyounoosung@gmail.com>
2021-09-01 22:42:07 +0900

    Update ProductPage.tsx

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -130,17 +130,18 @@ function ProductPage({ product }: { product: Product }) {
                   <strong className="mr-1">1,034</strong>리뷰
                 </a>
               </div>
-              <div className="flex">
-                <p>
-                  ShareUrl: {shareUrl}/products/{product ? product.slug?.current : ''}
-                </p>
-                <FacebookShareButton url={`${shareUrl}/products/${product.slug}`} quote={product?.title} className="">
-                  <FacebookIcon size={24} round />
-                </FacebookShareButton>
-                <TwitterShareButton url={`${shareUrl}/products/${product.slug}`} className="ml-2">
-                  <TwitterIcon size={24} round />
-                </TwitterShareButton>
-                {/* <button type="button" className="btn-kakaotalk">
+              {product && product.slug?.current && (
+                <div className="flex">
+                  <p>
+                    ShareUrl: {shareUrl}/products/{product ? product.slug?.current : ''}
+                  </p>
+                  <FacebookShareButton url={`${shareUrl}/products/${product.slug}`} quote={product?.title} className="">
+                    <FacebookIcon size={24} round />
+                  </FacebookShareButton>
+                  <TwitterShareButton url={`${shareUrl}/products/${product.slug}`} className="ml-2">
+                    <TwitterIcon size={24} round />
+                  </TwitterShareButton>
+                  {/* <button type="button" className="btn-kakaotalk">
                   <svg className="w-10 h-10" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
                     <path d="m235.178 216.211a10.855 10.855 0 0 0 -20.323 0l-20.925 55.989a6.971 6.971 0 0 0 13.059 4.88l3.341-10.085h28.046a6.464 6.464 0 0 0 1.284-.129l3.384 10.214a6.97 6.97 0 1 0 13.056-4.88zm-18.678 38.214 9.921-29.945 9.921 29.945z"/>
                     <path d="m202.965 216.239a6.971 6.971 0 0 0 -6.97-6.971h-42.572a6.971 6.971 0 0 0 0 13.942h14.315v51.427a6.97 6.97 0 1 0 13.94 0v-51.427h14.322a6.971 6.971 0 0 0 6.965-6.971z"/>
@@ -149,7 +150,8 @@ function ProductPage({ product }: { product: Product }) {
                     <path d="m256 0c-141.385 0-256 114.615-256 256s114.615 256 256 256 256-114.615 256-256-114.615-256-256-256zm0 358.935a185.433 185.433 0 0 1 -28.708-2.225l-57.987 40.667a3.674 3.674 0 0 1 -5.663-3.941l13.779-52.516c-40.928-20.55-68.088-56.675-68.088-97.814 0-63.971 65.667-115.829 146.667-115.829s146.667 51.858 146.667 115.829-65.667 115.829-146.667 115.829z"/>
                   </svg>
                 </button> */}
-              </div>
+                </div>
+              )}
             </div>
           </div>
 
