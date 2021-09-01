2d781bbaaa6d381a78f18e05b4b72dc814282995
hyounoo <hyounoosung@gmail.com>
2021-09-01 22:37:28 +0900

    product page

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { urlFor, PortableText, getClient } from '../lib/sanity'
 import { Box, Card, CardContent, Icon, Typography, TextareaAutosize } from '@material-ui/core'
 import { Rating } from '@material-ui/lab'
-import { FacebookShareButton, TwitterShareButton, FacebookIcon, TwitterIcon } from "react-share"
+import { FacebookShareButton, TwitterShareButton, FacebookIcon, TwitterIcon } from 'react-share'
 import { number } from 'yup'
 import clsx from 'clsx'
 import { InView } from 'react-intersection-observer'
@@ -40,7 +40,7 @@ function ProductPage({ product }: { product: Product }) {
     3.5: 'face-07',
     4: 'face-08',
     4.5: 'face-09',
-    5: 'face-10',
+    5: 'face-10'
     // 0: 'bad',
     // 0.5: 'Useless',
     // 1: 'Useless+',
@@ -59,13 +59,13 @@ function ProductPage({ product }: { product: Product }) {
   // for Review
   const [activeWriteReview, setActiveWriteReview] = useState(false)
   const handleWriteReview = (ev: any) => {
-    setActiveWriteReview(true);
+    setActiveWriteReview(true)
     setTimeout(() => {
-      const textareaElem = document.querySelector('#textareaReview');
-      if(textareaElem) {
-        textareaElem.focus();
+      const textareaElem = document.querySelector('#textareaReview') as HTMLElement
+      if (textareaElem) {
+        textareaElem.focus()
       }
-    }, 100);
+    }, 100)
   }
 
   // for Intersection Observer
@@ -73,41 +73,39 @@ function ProductPage({ product }: { product: Product }) {
   const [tabReview, setTabReview] = useState(false)
   const [tabEtc, setTabEtc] = useState(false)
   const handleIntersectionTab = (inView: any, entry: any) => {
-    const elem = entry.target;
-    const elemId = elem.getAttribute('id');
+    const elem = entry.target
+    const elemId = elem.getAttribute('id')
     // console.log('Inview:', inView);
     // console.log(elemId);
 
-    if(inView) {
-      if(elemId === 'productDetail') {
-        setTabDetail(true);
-        setTabReview(false);
-        setTabEtc(false);
-      }
-      else if(elemId === 'productReview') {
-        setTabDetail(false);
-        setTabReview(true);
-        setTabEtc(false);
-      }
-      else if(elemId === 'productEtc') {
-        setTabDetail(false);
-        setTabReview(false);
-        setTabEtc(true);
+    if (inView) {
+      if (elemId === 'productDetail') {
+        setTabDetail(true)
+        setTabReview(false)
+        setTabEtc(false)
+      } else if (elemId === 'productReview') {
+        setTabDetail(false)
+        setTabReview(true)
+        setTabEtc(false)
+      } else if (elemId === 'productEtc') {
+        setTabDetail(false)
+        setTabReview(false)
+        setTabEtc(true)
       }
     }
   }
 
-  // const shareUrl = path까지는 알아봤는데 HOST를 가져오는 방법을 모르겠어요 ㅠㅠ;
+  const shareUrl = typeof window !== 'undefined' && window.location.hostname ? window.location.hostname : ''
 
   const writeOnlyNumber = (ev: any) => {
     // Remove invalid characters
-    var sanitized = ev.currentTarget.value.replace(/[^-.0-9]/g, '');
+    var sanitized = ev.currentTarget.value.replace(/[^-.0-9]/g, '')
     // Remove non-leading minus signs
-    sanitized = sanitized.replace(/(.)-+/g, '$1');
+    sanitized = sanitized.replace(/(.)-+/g, '$1')
     // Remove the first point if there is more than one
-    sanitized = sanitized.replace(/\.(?=.*\.)/g, '');
+    sanitized = sanitized.replace(/\.(?=.*\.)/g, '')
     // Update value
-    ev.currentTarget.value = sanitized;
+    ev.currentTarget.value = sanitized
   }
 
   return (
@@ -128,20 +126,18 @@ function ProductPage({ product }: { product: Product }) {
               <div className="flex">
                 {/* 아래 Rating은 현재 상품의 별점으로 value에 적용한 4.5는 임시데이터입니다. */}
                 <Rating name="read-only" precision={0.5} value={4.5} readOnly />
-                <a href="#productReview" className="ml-1"><strong className="mr-1">1,034</strong>리뷰</a>
+                <a href="#productReview" className="ml-1">
+                  <strong className="mr-1">1,034</strong>리뷰
+                </a>
               </div>
               <div className="flex">
-                <FacebookShareButton
-                  // url={shareUrl}
-                  url="https://google.com"
-                  quote={product?.title}
-                  className="">
+                <p>
+                  ShareUrl: {shareUrl}/products/{product ? product.slug?.current : ''}
+                </p>
+                <FacebookShareButton url={`${shareUrl}/products/${product.slug}`} quote={product?.title} className="">
                   <FacebookIcon size={24} round />
                 </FacebookShareButton>
-                <TwitterShareButton
-                  // url={shareUrl}
-                  url="https://google.com"
-                  className="ml-2">
+                <TwitterShareButton url={`${shareUrl}/products/${product.slug}`} className="ml-2">
                   <TwitterIcon size={24} round />
                 </TwitterShareButton>
                 {/* <button type="button" className="btn-kakaotalk">
@@ -159,7 +155,9 @@ function ProductPage({ product }: { product: Product }) {
 
           <CardContent className="flex flex-col justify-between w-full px-0 py-0 md:pr-6 md:pb-10">
             <div aria-label="top">
-              <Typography variant="h5" component="h3">{product?.title}</Typography>
+              <Typography variant="h5" component="h3">
+                {product?.title}
+              </Typography>
               <div className="flex items-center">
                 <Icon className="mr-1">paid</Icon>
                 <span className="mr-1 text-2xl font-bold">
@@ -175,14 +173,23 @@ function ProductPage({ product }: { product: Product }) {
             <div aria-label="bottom">
               <div className="mt-2 flex justify-between">
                 <div className="flex align-top items-center border border-gray-200">
-                  <button type="button" disabled={count <= 1} className="border-r p-2 text-gray-400 hover:text-gray-600" onClick={() => handleCount(-1)}>
+                  <button
+                    type="button"
+                    disabled={count <= 1}
+                    className="border-r p-2 text-gray-400 hover:text-gray-600"
+                    onClick={() => handleCount(-1)}
+                  >
                     <Icon className="align-top">remove</Icon>
                   </button>
 
                   {/* 수량을 직접 입력할 수 있게 input을 썼는데 value에 count를 적용하면 수정이 불가하네요 ㅠㅠ */}
                   <input type="text" className="w-10 text-center" value={count} onChange={writeOnlyNumber} />
 
-                  <button type="button" className="border-l p-2 text-gray-400 hover:text-gray-600" onClick={() => handleCount(1)}>
+                  <button
+                    type="button"
+                    className="border-l p-2 text-gray-400 hover:text-gray-600"
+                    onClick={() => handleCount(1)}
+                  >
                     <Icon className="align-top">add</Icon>
                   </button>
                 </div>
@@ -190,7 +197,9 @@ function ProductPage({ product }: { product: Product }) {
                 <div className="flex-1 flex justify-end items-center">
                   <Icon className="mr-1 text-blue-600">paid</Icon>
                   {/* <span>합계</span> */}
-                  <span className="mr-1 text-2xl font-bold">{count <= 1 ? product?.defaultProductVariant?.price : product?.defaultProductVariant?.price * count}</span>
+                  <span className="mr-1 text-2xl font-bold">
+                    {count <= 1 ? product?.defaultProductVariant?.price : product?.defaultProductVariant?.price * count}
+                  </span>
                   <span className="mt-1 text-gray-500 text-lg italic">CHOO</span>
                 </div>
               </div>
@@ -198,8 +207,12 @@ function ProductPage({ product }: { product: Product }) {
                 <button type="button" className="mr-1 px-2" onClick={() => setSelectedLike(!selectedLike)}>
                   <Icon className={selectedLike ? 'text-red-600 transition-all' : 'transition-all'}>favorite</Icon>
                 </button>
-                <button type="button" className="flex-1 border border-blue-600 text-blue-600">장바구니 담기</button>
-                <button type="button" className="flex-1 bg-blue-600 text-white">바로구매</button>
+                <button type="button" className="flex-1 border border-blue-600 text-blue-600">
+                  장바구니 담기
+                </button>
+                <button type="button" className="flex-1 bg-blue-600 text-white">
+                  바로구매
+                </button>
               </div>
             </div>
           </CardContent>
@@ -207,24 +220,34 @@ function ProductPage({ product }: { product: Product }) {
         </div>
       </Card>
 
-      <div className="h-20">
-
-      </div>
+      <div className="h-20"></div>
 
       <ul id="tabProduct" className="tab-product lg:sticky flex border border-blue-600 h-12 text-blue-600 bg-white">
-        <li className={clsx(tabDetail && "active", "flex-1 flex justify-center items-center border-r border-blue-600")} >
-          <a href="#productDetail" className="flex justify-center items-center w-full h-full">상품상세</a>
+        <li className={clsx(tabDetail && 'active', 'flex-1 flex justify-center items-center border-r border-blue-600')}>
+          <a href="#productDetail" className="flex justify-center items-center w-full h-full">
+            상품상세
+          </a>
         </li>
-        <li className={clsx(tabReview && "active", "flex-1 flex justify-center items-center border-r border-blue-600")} >
-          <a href="#productReview" className="flex justify-center items-center w-full h-full">상품후기</a>
+        <li className={clsx(tabReview && 'active', 'flex-1 flex justify-center items-center border-r border-blue-600')}>
+          <a href="#productReview" className="flex justify-center items-center w-full h-full">
+            상품후기
+          </a>
         </li>
-        <li className={clsx(tabEtc && "active", "flex-1 flex justify-center items-center border-blue-600")} >
-          <a href="#productEtc" className="flex justify-center items-center w-full h-full">기타안내</a>
+        <li className={clsx(tabEtc && 'active', 'flex-1 flex justify-center items-center border-blue-600')}>
+          <a href="#productEtc" className="flex justify-center items-center w-full h-full">
+            기타안내
+          </a>
         </li>
       </ul>
 
       {/* 상품상세 */}
-      <InView as="section" threshold={0.1} onChange={handleIntersectionTab} id="productDetail" className="section mt-20 lg:pt-12">
+      <InView
+        as="section"
+        threshold={0.1}
+        onChange={handleIntersectionTab}
+        id="productDetail"
+        className="section mt-20 lg:pt-12"
+      >
         <div className="section__inner">
           <h3 className="title text-lg">상품상세</h3>
           <div className="my-4">동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세</div>
@@ -265,17 +288,26 @@ function ProductPage({ product }: { product: Product }) {
       </InView>
 
       {/* 상품후기 */}
-      <InView as="section" threshold={0.5} onChange={handleIntersectionTab} id="productReview" className="section mt-20 lg:pt-12">
+      <InView
+        as="section"
+        threshold={0.5}
+        onChange={handleIntersectionTab}
+        id="productReview"
+        className="section mt-20 lg:pt-12"
+      >
         <div className="section__inner">
           <h3 className="title text-lg flex justify-between items-center">
-            <div>상품후기<span className="ml-1 text-xs font-light">(1,306건)</span></div>
+            <div>
+              상품후기<span className="ml-1 text-xs font-light">(1,306건)</span>
+            </div>
 
             {/* 아래 버튼은 로그인 사용자 또는 후기 작성권한이 있는 경우 노출 */}
-            <div className={activeWriteReview ? "hidden" : ""}>
+            <div className={activeWriteReview ? 'hidden' : ''}>
               <button
                 type="button"
                 className="flex items-center border border-black p-1 hover:text-white hover:bg-black"
-                onClick={handleWriteReview}>
+                onClick={handleWriteReview}
+              >
                 <Icon className="text-base">create</Icon>
                 <span className="text-sm ml-1">후기 작성하기</span>
               </button>
@@ -288,20 +320,23 @@ function ProductPage({ product }: { product: Product }) {
             </div>
             <div className="flex">
               <span className="mr-2 text-lg">상품 평점</span>
-              <div><strong className="text-lg">4.5</strong> / 5</div>
+              <div>
+                <strong className="text-lg">4.5</strong> / 5
+              </div>
             </div>
           </div>
 
-          <form className={clsx(!activeWriteReview && "hidden", "flex flex-wrap flex-col lg:flex-row lg:mt-4")}>
+          <form className={clsx(!activeWriteReview && 'hidden', 'flex flex-wrap flex-col lg:flex-row lg:mt-4')}>
             <div className="flex-1">
-              <TextareaAutosize 
+              <TextareaAutosize
                 id="textareaReview"
                 className="flex-1 w-full border border-black border-t-0 lg:border-t rounded-none p-2 shadow-none outline-none appearance-none"
                 maxRows={10}
                 minRows={3}
                 maxLength={300}
                 aria-label="후기 작성 입력란"
-                placeholder="상품 후기를 작성해주세요 :)" />
+                placeholder="상품 후기를 작성해주세요 :)"
+              />
             </div>
             <div className="flex justify-center items-center px-4">
               <div className="flex flex-col justify-center items-center">
@@ -313,19 +348,23 @@ function ProductPage({ product }: { product: Product }) {
                   precision={0.5}
                   defaultValue={0}
                   onChange={(ev, newValue) => {
-                    setRateValue(newValue);
-                  }} 
+                    setRateValue(newValue)
+                  }}
                   onChangeActive={(ev, newHover) => {
-                    setHover(newHover);
+                    setHover(newHover)
                   }}
                 />
               </div>
-              {rateValue !== null && <div className={clsx("ml-4 face", ratingLabels[hover !== -1 ? hover : rateValue])}></div>}
+              {rateValue !== null && (
+                <div className={clsx('ml-4 face', ratingLabels[hover !== -1 ? hover : rateValue])}></div>
+              )}
             </div>
           </form>
 
           <div className="flex justify-center mt-4 h-12">
-            <button type="button" className="flex-1 lg:flex-initial w-40 bg-blue-600 text-white">등록</button>
+            <button type="button" className="flex-1 lg:flex-initial w-40 bg-blue-600 text-white">
+              등록
+            </button>
           </div>
 
           <div className="my-4">남산위에 저소나무 철갑을 두른듯 바람서리 불변함은 우리 기상일세</div>
@@ -349,7 +388,13 @@ function ProductPage({ product }: { product: Product }) {
       </InView>
 
       {/* 기타안내 */}
-      <InView as="section" threshold={0.5} onChange={handleIntersectionTab} id="productEtc" className="section mt-20 lg:pt-12">
+      <InView
+        as="section"
+        threshold={0.5}
+        onChange={handleIntersectionTab}
+        id="productEtc"
+        className="section mt-20 lg:pt-12"
+      >
         <div className="section__inner">
           <h3 className="title text-lg">기타안내</h3>
           <div className="my-4">가을하늘 공활한데 높고 구름없이 밝은달은 우리가슴 일편 단심일세</div>
